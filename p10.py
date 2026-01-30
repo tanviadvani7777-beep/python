45dc0b46b4b18ef86dadff9e3853eea6111a8314
tanviadvani7777-beep <tanviadvani7777@gmail.com>
2026-01-30 12:35:38 +0530

    Add files via upload

---
+++ p10.py
@@ -0,0 +1,7 @@
+10.Write a program to create 4 lambda functions which shall accept 2 numbers and one arithmetic operator. As per arithmetic operator related lambda functions shall be invoked.
+
+
+check = lambda x: "positive" if x>0 else "negetive" if x<0 else "zero"
+print (check(5))
+print (check(-90))
+print (check(0))
