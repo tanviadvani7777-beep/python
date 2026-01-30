33bb06ae54468aca25980322d38f7ec73f8f6a39
tanviadvani7777-beep <tanviadvani7777@gmail.com>
2026-01-30 12:35:23 +0530

    Add files via upload

---
+++ p9.py
@@ -0,0 +1,6 @@
+#. Write a program to create a function which accepts 2 numbers and one arithmetic operator. Return the answer accordingly.
+a = 999
+b = 348
+#adding two numbers
+res = a+b
+print(res)
