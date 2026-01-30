e4550de5b3abb76467d46ffc51e839e260afb619
tanviadvani7777-beep <tanviadvani7777@gmail.com>
2026-01-30 12:34:12 +0530

    Add files via upload

---
+++ p6.py
@@ -0,0 +1,12 @@
+#6. Define a procedure histogram() that takes a list of integers and prints a
+#histogram to the screen. For example, histogram([4, 9, 7]) should print
+#the following:
+#****
+#*********
+#*******
+
+
+def histogram(items):
+    for n in items:
+       print('*' *n)
+histogram([4,9,7])
