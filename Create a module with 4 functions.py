cb3e2107ee12ecebffe1cc431e4656e0359863ef
tanvi advani <tanviadvani7777@gmail.com>
2026-03-13 11:25:46 +0530

    module with 4 function

---
+++ Create a module with 4 functions.py
@@ -0,0 +1,8 @@
+#Create a module with 4 functions of your choice. Import and use the functions using module in different ways.
+from math import sqrt, factorial
+print(sqrt(16))
+print(factorial(6))
+import math as m
+print(m.pi)
+import random
+print(random.randint(1, 5))
